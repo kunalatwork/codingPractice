5f5333fd8bcc0b2d88f2552c51d8fb135b527272
Kunal Rawat <kunal.rawat3123@gmail.com>
2020-03-08 19:16:34 +0530

    practice array sum Signed-off-by: Kunal Rawat <kunal.rawat3123@gmail.com>

---
+++ src/Java_question/SumOfArray.java
@@ -0,0 +1,13 @@
+package Java_question;
+
+public class SumOfArray {
+	public static void main(String[] args) {
+		int[] a = {2,4,6,8,9};
+		int sum=0;
+		for (int i =0;i<a.length;i++)
+		{
+			sum+=a[i];
+		}
+		System.out.println(sum);
+	}
+}
